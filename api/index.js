0834febd472ff8c58f659a5183851ce5429898ae
pierreSNC <pierresenechal7@gmail.com>
2024-04-27 19:46:16 +0200

    fix: add dialect on .env.dist

--- api/index.js
+++ api/index.js
@@ -9,14 +9,6 @@ require('./src/controllers/PostsController')(app);
 require('./src/controllers/CategoriesController')(app);
 require('./src/controllers/AuthorsController')(app);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.post('/', (req, res) => {
-    res.send('Got a POST request')
-})
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
