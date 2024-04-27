4ff21b76b52388aebc9baa970320529e5160308b
pierreSNC <pierresenechal7@gmail.com>
2024-04-27 16:34:23 +0200

    feat: connection to database

---
+++ api/index.js
@@ -0,0 +1,17 @@
+require('./database');
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
